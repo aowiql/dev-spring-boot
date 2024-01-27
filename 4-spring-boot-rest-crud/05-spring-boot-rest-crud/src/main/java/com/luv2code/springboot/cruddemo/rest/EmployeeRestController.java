9682056eb801e743c983bf0eb9ee29cd55926d7d
qweewq <seongkh15@naver.com>
2024-01-27 17:06:24 +0900

    Rest POST

--- 4-spring-boot-rest-crud/05-spring-boot-rest-crud/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 4-spring-boot-rest-crud/05-spring-boot-rest-crud/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,10 +3,7 @@ package com.luv2code.springboot.cruddemo.rest;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,5 +33,15 @@ public class EmployeeRestController {
         }
 
         return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
     }
 }
