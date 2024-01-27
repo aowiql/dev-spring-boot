f0d8d136e1d30cecc445782118edf5de008f5569
qweewq <seongkh15@naver.com>
2024-01-27 16:49:01 +0900

    service crud

--- 4-spring-boot-rest-crud/04-spring-boot-dao-crud/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ 4-spring-boot-rest-crud/04-spring-boot-dao-crud/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,9 +1,9 @@
 package com.luv2code.springboot.cruddemo.service;
 
 import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAOJpaImpl;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,5 +19,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }
