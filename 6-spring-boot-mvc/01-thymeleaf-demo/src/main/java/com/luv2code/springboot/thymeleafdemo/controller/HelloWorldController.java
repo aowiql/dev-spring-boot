b89241a7291a520a96c2494d275d193a3e37b7a3
qweewq <seongkh15@naver.com>
2024-02-10 18:58:43 +0900

    add data

--- 6-spring-boot-mvc/01-thymeleaf-demo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 6-spring-boot-mvc/01-thymeleaf-demo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +15,20 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        String theName = request.getParameter("studentName");
+
+        theName = theName.toUpperCase();
+
+        String result = "Yo! " + theName;
+
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 
