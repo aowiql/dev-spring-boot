fb5194af1bd90b8bec90fb26275421d02d990245
qweewq <seongkh15@naver.com>
2024-02-11 18:01:50 +0900

    Text Fields 2

--- 6-spring-boot-mvc/02-textfield/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 6-spring-boot-mvc/02-textfield/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.luv2code.springboot.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -16,5 +18,11 @@ public class StudentController {
         theModel.addAttribute("student", theStudent);
 
         return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+
+        return "student-confirmation";
     }
 }
